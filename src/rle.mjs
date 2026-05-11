9898f6763157a5b73e4c422ac6e3e0415172c63f
Kilian Huegler <kilianhuegler@icloud.com>
2026-05-11 21:07:04 +0300

    Added test multiple live cells

--- src/rle.mjs
+++ src/rle.mjs
@@ -1,3 +1,14 @@
-export function parseRle(s) {
-  return new Set(["0,0"]);
+export function parseRle(input) {
+  const liveCells = new Set();
+  const lines = input.split("\n")[1];
+
+  let x = 0;
+  for (const line of lines) {
+    if (line === "o") {
+      liveCells.add(`${x},0`);
+      x++;
+    }
+  }
+
+  return liveCells;
 }
--- test/rle.test.mjs
+++ test/rle.test.mjs
@@ -2,10 +2,16 @@ import { describe, test } from "vitest";
 import { expect } from "chai";
 import { parseRle } from "../src/rle.mjs";
 
-describe("rle", () => {
+describe("RLE Parser", () => {
   test("single live cell", () => {
     const rle = "x = 1, y = 1\no!";
 
     expect(parseRle(rle)).to.deep.equal(new Set(["0,0"]));
   });
+
+  test("multiple live cells", () => {
+    const rle = "x = 3, y = 1\nooo!";
+
+    expect(parseRle(rle)).to.deep.equal(new Set(["0,0", "1,0", "2,0"]));
+  })
 });
